9a85cfe3cc9cecc04d8e17de35eb0aaa521c38dd
pkyleZbook <kyle.park@ultradent.com>
2023-11-01 11:59:12 -0600

    made read and write functions easier to handle errors

--- main.cpp
+++ main.cpp
@@ -19,43 +19,56 @@ class data{
 };
 
 template <typename D>
-void saveDataTypeToFile(D*data, const std::string& fileName){
+int saveDataTypeToFile(D*data, const std::string& fileName){
     auto file = std::fstream(fileName,std::ios::out | std::ios::binary);
     if(!file.is_open()){
 	    std::cout << "Failed to Open File '" + fileName + "'" << std::endl;
+	    return 1;
     }
     file.write((char*)data, sizeof(*data));
     file.close();
+    return 0;
 }
 
 template <typename D>
-void readDataFromFile(D*data, const std::string&fileName){
+int readDataFromFile(D*data, const std::string&fileName){
     std::ifstream file(fileName, std::ios::out | std::ios::binary);
     if(!file){
         std::cout <<"Failed to Open File"<< std::endl;
+	return 1;
     }
     file.read((char*)data,sizeof(*data));
     file.close();
     if(!file.good()){
         std::cout << "failed to read file" << std::endl;
+	return 1;
     }
+    return 0;
 }
 int main(){
+	//Create Data To save out
 	data d; 
 	d.set_iVal(1234);
 	d.getData();
-	saveDataTypeToFile(&d, "testFile.dat");
 
+	//Save Data out to file
+	if(saveDataTypeToFile(&d, "testFile.dat")){
+		//HANDLE ERRORS
+	}
+
+	//Create Data to Read into
 	data readD;
 	std::cout << "readD Before Reading: " << std::endl;
 	readD.getData();
 
-	readDataFromFile(&readD, "testFile.dat");
+	//Read Data from filee
+	if(readDataFromFile(&readD, "testFile.dat")){
+		//HANDLE ERRORS
+	}
 
+	//Prove we successfully changed the data
 	std::cout << "readD After Reading: " <<std::endl;
 	readD.getData();
-
-
 
 	return 1;
 }
